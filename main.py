1a70e20b9bedf6bc99afcd5868a16445e4f5f442
landbroken <445299258@qq.com>
2021-10-09 23:53:03 +0800

    add main

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 LinYulong. All Rights Reserved
+#
+# @Time    : 2021/10/9 23:10
+# @Author  : LinYulong
+
+if __name__ == "__main__":
+    print("123")
