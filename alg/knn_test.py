c42ae4a7cee1d206de54cdd72e7e5c13472df195
landbroken <445299258@qq.com>
2021-10-09 23:11:24 +0800

    add copyright

--- alg/knn_test.py
+++ alg/knn_test.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 LinYulong. All Rights Reserved
+#
+# @Time    : 2021/10/9 23:10
+# @Author  : LinYulong
+
 import unittest
 
 from numpy import array
