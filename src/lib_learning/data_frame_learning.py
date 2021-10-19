61c5832eda732bec8ecd0c507ff61946ff7b5cb9
landbroken <445299258@qq.com>
2021-10-19 08:12:56 +0800

    prepare for simplify group

--- src/lib_learning/data_frame_learning.py
+++ src/lib_learning/data_frame_learning.py
@@ -5,7 +5,7 @@
 #
 # @Time    : 2021/10/12
 # @Author  : LinYulong
-
+import numpy
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -20,3 +20,9 @@ print(df2)
 df3 = df.iloc[:, 0]  # 全部行，[0,0]列
 print(df3)
 
+df4 = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]], columns=['CHD1', 'CHD2', 'CHD3'])
+np5 = numpy.zeros(shape=(4, 2))
+df5 = pd.DataFrame(np5, columns=['1', '2'])
+df5["1"] = df4["CHD1"] + df4["CHD2"]
+df5["2"] = df4["CHD3"]
+print(df5)
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@
 #
 # @Time    : 2021/10/9 23:10
 # @Author  : LinYulong
+import numpy
 import pandas
 
 from src.excel import excel_helper
@@ -12,9 +13,33 @@ from src.alg import cross_verify, math_helper
 from train import train
 
 
-def simplify_table(filepath: str):
+def merge_chd(df: pandas.DataFrame) -> pandas.DataFrame:
+    line_size = df.index.size
+    np_zero = numpy.zeros(shape=(line_size, 5))
+    ret = pandas.DataFrame(np_zero, columns=['1', '2', '3', '4', '5'])
+    ret["1"] = (df["CHD1"] + df["CHD2"] + df["CHD9"]) / 3
+    ret["2"] = (df["CHD3"] + df["CHD4"] + df["CHD5"] + df["CHD6"] + df["CHD7"]) / 5
+    ret["3"] = df["CHD11"]
+    ret["4"] = (df["CHD8"] + df["CHD10"]) / 2
+    ret["5"] = (df["CHD12"] + df["CHD13"] + df["CHD14"]) / 3
+    return ret
+
+
+def merge_hy(df: pandas.DataFrame) -> pandas.DataFrame:
+    line_size = df.index.size
+    np_zero = numpy.zeros(shape=(line_size, 4))
+    ret = pandas.DataFrame(np_zero, columns=['1', '2', '3', '4'])
+    ret["1"] = (df["HY1"] + df["HY2"] + df["HY3"] + df["HY7"] + df["HY10"]) / 5
+    ret["2"] = (df["HY4"] + df["HY5"] + df["HY6"] + df["HY8"]) / 4
+    ret["3"] = (df["HY9"] + df["HY12"]) / 2
+    ret["4"] = (df["HY11"] + df["HY13"]) / 2
+    return ret
+
+
+def simplify_table(filepath: str, merge_fun):
     min_err_percent = 0
     df: pandas.DataFrame = excel_helper.read_resource(filepath)
+    #df = merge_fun(df)
     # 简化计算表格
     question_size = df.columns.size
     while question_size > 1:
@@ -34,7 +59,7 @@ def simplify_table(filepath: str):
         question_size -= 1
 
 
-simplify_table("/冠心病.xlsx")
+simplify_table("/冠心病.xlsx", merge_chd)
 # simplify_table("/高血压.xlsx")
 
 
