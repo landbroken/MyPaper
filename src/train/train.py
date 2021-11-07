ddfd7b781f26156c320819112a9ee64d0083dd75
landbroken <445299258@qq.com>
2021-11-07 12:02:40 +0800

    特异度和敏感度计算

--- src/train/train.py
+++ src/train/train.py
@@ -105,6 +105,10 @@ def to_negative_and_positive_table(df: pandas.DataFrame) -> pandas.DataFrame:
     return ret
 
 
+def sub_table(df: pandas.DataFrame) -> pandas.DataFrame:
+    pass
+
+
 def table_sort(df: pandas.DataFrame, sort_fun) -> pandas.DataFrame:
     """
     对表格排序
@@ -130,3 +134,27 @@ def get_question_mark(np: numpy.ndarray):
     :param np:
     :return:
     """
+
+
+def cal_sensitivity(tp: int, fp: int) -> float:
+    """
+    灵敏度计算。
+    灵敏度是指测试正确检测出患有这种疾病的患者的能力
+    https://en.wikipedia.org/wiki/Sensitivity_and_specificity
+    :param tp: 真阳性人数
+    :param fp: 假阳性人数
+    :return:
+    """
+    tpr = tp / (tp + fp * 1.0)  # tp rate
+    return tpr
+
+
+def cal_specificity(tn: int, fp: int) -> float:
+    """
+    特异性计算
+    :param tn: 真阴性人数
+    :param fp: 假阳性人数
+    :return: tnr: 特异性/选择性/真阴性率
+    """
+    tnr = tn / (tn + fp * 1.0)
+    return tnr
